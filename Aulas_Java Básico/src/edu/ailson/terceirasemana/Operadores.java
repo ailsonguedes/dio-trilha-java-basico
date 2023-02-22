c87bf46c75b6c1e1c3b94a337ee16ef3dc294bc9
ailsonguedes <ailsonsixseven@gmail.com>
2023-02-22 15:09:45 -0300

    220220231509

--- Aulas_Java Básico/src/edu/ailson/terceirasemana/Operadores.java
+++ Aulas_Java Básico/src/edu/ailson/terceirasemana/Operadores.java
@@ -4,7 +4,38 @@ package edu.ailson.terceirasemana;
 public class Operadores {
     public static void main (String[] args) {
         // Operadores - 1:
+        // Atribuição: = 
+        // Aritméticos: 
+        // Soma: + Também serve para concatenar strings
+        // Subtrção: -
+        // Multiplicação: *
+        // Divisão: /
+        // Módulo: %
+        //
 
+        // Concatenação: 
+        String fullName = "first name" + " last name";
+        String concateName = fullName;
+
+        System.out.printf("Result: %s\n", concateName);
+
+        String concatenacao = "?";
+
+        concatenacao =  1 + 1 + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao =  1 + "1" + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao =  "1" + 1 + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao =  "1" + (1 + 1 + 1);
+
+        System.out.println(concatenacao);
 
     }
 }
